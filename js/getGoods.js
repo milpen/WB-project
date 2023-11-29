55c802a825214a1ba722867c7f27bf9c6881b449
milpen <milpen@mail.ru>
2023-11-29 22:41:26 +0500

    day three

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,29 +1,49 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link');
 
-    const getData = () => {
+    const renderGoods = (goods) => {
+        console.log(goods);
+    }
+
+    const getData = (value, category) => {
         fetch('/db/db.json')
         .then((res) => res.json())
         .then((data) => {
-            console.log(data);
+            //console.log(data);
+            const array = data.filter((item) => item[category] === value);
+        localStorage.setItem('goods', JSON.stringify(array));
+
+        if(window.location.pathname !== "goods.html") {
+            window.location.href = '/goods.html'
+        } else {
+            renderGoods(array)
+        }
+
+
         })
     }
 links.forEach((link) => {
     link.addEventListener('click', (event) =>{
         event.preventDefault();
-        getData();
+        const linkValue = link.textContent;
+        const category = link.dataset.field;
+
+        getData(linkValue, category);
     })
 })
 
 //localStorage.setItem('goods', JSON.stringify({name: 'all'}));
-localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4, 5]));
+//localStorage.setItem('goods', JSON.stringify([1, 2, 3, 4, 5]));
 
 //console.log(JSON.parse(localStorage.getItem('goods')));
-const goods =JSON.parse(localStorage.getItem('goods'));
-console.log(1, goods);
+//const goods =JSON.parse(localStorage.getItem('goods'));
+//console.log(1, goods);
 
 //удаление из LocalStorage
-localStorage.removeItem('goods');
+//localStorage.removeItem('goods');
 //console.log(2, localStorage);
+if (localStorage.getItem('goods') && window.location.pathname === "/goods.html") {
+    renderGoods(JSON.parse(localStorage.getItem('goods')))
+}
 }
 getGoods();
